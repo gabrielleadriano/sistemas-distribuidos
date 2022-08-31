12b7413b0a4cd7c6f604eb426477e1098279b221
Gabrielle Adriano <gabrielle.a.adriano@gmail.com>
2022-08-31 19:54:14 -0300

    servidor atualizado

--- exclusao-mutua/ServidorTCP.java
+++ exclusao-mutua/ServidorTCP.java
@@ -1,60 +1,71 @@
 import java.net.*;
 import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
-
-public class ServidorTCP 
-{
+public class ServidorTCP {
 	public static void main(String[] args) throws Exception {
 		String numeroPorta;
 		ServerSocket serverSocket;
+		Socket newClientSocket;
 		Socket clientSocket;
 		PrintWriter out;
 		BufferedReader in;
 		String comando;
-		
+		int currentIndex = 0;
+		List<Socket> clientList = new ArrayList<>();
+		String msgBoard = "";
+
 		/* Parametros */
 		numeroPorta = args[0];
 
 		/* Inicializacao do server socket TCP */
-		serverSocket = new ServerSocket(
-				new Integer (numeroPorta).intValue());
+		serverSocket = new ServerSocket(new Integer(numeroPorta).intValue());
 
-		while (true){
+		while (true) {
 			/* Espera por um cliente */
-			clientSocket = serverSocket.accept();
-			System.out.println ("Novo cliente: "+serverSocket.toString());
+			newClientSocket = serverSocket.accept();
+			clientList.add(newClientSocket);
+
+			if (currentIndex < clientList.size() - 1) {
+				System.out.println("dentro if");
+				String msgAwait = "AGUARDE, VOCÊ ESTÁ NA POSICAO: " + clientList.size();
+
+				out = new PrintWriter(newClientSocket.getOutputStream(), true);
+				out.println(msgAwait);
+			}
+			clientSocket = clientList.get(currentIndex);
+
+			System.out.println("Novo cliente: " + serverSocket.toString());
 
 			/* Preparacao dos fluxos de entrada e saida */
-			out = new PrintWriter(clientSocket.getOutputStream(),
-					true);
-			in = new BufferedReader(new InputStreamReader(
-					clientSocket.getInputStream()));
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
 			/* Recuperacao dos comandos */
-			while ((comando = in.readLine()) != null) {	
-				System.out.println ("Comando recebido: ["+ comando+"]");
+			while ((comando = in.readLine()) != null) {
+				System.out.println("Comando recebido: [" + comando + "]");
 				/* Se comando for "HORA" */
-				if (comando.equals ("HORA")){
-					/* Prepara a hora para envio */
-					String hora = new SimpleDateFormat("d MMM yyyy HH:mm:ss").format(new Date ());
-					/* Escreve na saida a 'hora' */
-					out.println (hora);
-				}else if (comando.equals ("FIM")){
+				if (comando.equals("LER")) {
+					out.println(msgBoard);
+				}else if(comando.contains("ESCREVER:")) {
+					msgBoard += "\n" + comando.split(":")[1];
 					break;
-				}else{
-					out.println ("Comando Desconhecido");
+				}
+				else if (comando.equals("SAIR")) {
+					currentIndex++;
+					break;
+				} else {
+					out.println("Comando Desconhecido");
 				}
 			}
 			/* Finaliza tudo */
-			System.out.print ("Cliente desconectando... ");
+			System.out.print("Cliente desconectando... ");
 			out.close();
 			in.close();
 			clientSocket.close();
-			System.out.println ("ok");
+			System.out.println("ok");
 		}
 
 	}
 }
-		
